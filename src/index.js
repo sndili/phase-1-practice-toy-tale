4f5d9ce50bea510f0fe15ee542c729fac703c537
Sylvia <ndilisylvia@gmail.com>
2022-08-28 00:51:05 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -12,4 +12,66 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  getAllToys()
+  createToy()
 });
+
+const toyCollection = document.querySelector("#toy-collection");
+
+function getAllToys(){
+  fetch("http://localhost:3000/toys")
+  .then(resp => resp.json())
+  .then(toyData => toyData.forEach(toy => renderToy(toy)))
+}
+
+function renderToy(toy) {
+  const card = document.createElement("div")
+  card.className = "card"
+  card.innerHTML = `
+    <h2>${toy.name}</h2>
+    <img src="${toy.image}" class="toy-avatar" />
+    <p>${toy.likes}</p>
+    <button class="like-btn" id="${toy.id}">Like</button>
+  `
+  toyCollection.appendChild(card)
+  card.querySelector(".like-btn").addEventListener("click", () => {
+    toy.likes += 1;
+    card.querySelector("p").innerText = toy.likes
+    likes(toy)
+  })
+}
+
+function createToy(){
+  document.querySelector(".add-toy-form").addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type" : "application/json",
+        Accept : "application/json"
+      },
+      body: JSON.stringify({
+        name: event.target[0].value,
+        image: event.target[1].value,
+        likes : 0
+      })
+    })
+    .then(resp => resp.json())
+    .then(renderToy)
+  })
+}
+
+function likes(toy) {
+  e.preventDefault()
+  fetch(`http://localhost:3000/toys/${toy.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type" : "application/json",
+      Accept : "application/json"
+    },
+    body: JSON.stringify(toy)
+  })
+  .then(resp => resp.json())
+  .then(toy => console.log(toy))
+} 
